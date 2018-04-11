13b5fb8f93b19e13bbf790832ef5efb6f1943a7a
angart <tissen.arthur@gmail.com>
2018-04-11 14:01:34 +0200

    Task 1.1

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Test test")
+console.log("Test test");
+
+var fs = require('fs');
+
+var staedte = fs.readFile(__dirname+"/staedte.json", function (err,data) {});
+
+console.log(JSON.stringify(staedte));
+/*
+fs.writeFile(__dirname+"/staedte.json",data ,function(err){
+
+});
+*/
