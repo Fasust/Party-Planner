581fe2f84ce3fbfa22a65e1ccc7ecda4b1f8c0e2
Sebastian Faust <sebastianf333@googlemail.com>
2018-04-11 15:11:18 +0200

    Task 3 Completed

--- main.js
+++ main.js
@@ -5,8 +5,17 @@ var chalk = require('chalk');
 fs.readFile(__dirname+'/Tasks/staedte.json', function(err, data) {
     if (err) throw err;
 
-    printCities(JSON.parse(data));
-    console.log("JSON Parsed")
+    var citJSON = JSON.parse(data);
+    sortCities(citJSON);
+
+
+    fs.writeFile(__dirname+'/Tasks/staedte_sorted.json', JSON.stringify(citJSON), function(err) {
+
+        printCities(citJSON);
+
+    });
+
+    console.log("JSON Parsed");
 
 });
 
@@ -15,10 +24,26 @@ console.log("Asynchrones sollte hier nach kommen");
 /*-----------------Functions--------------------*/
 
 function printCities(cit) {
+
     for(var i = 0; i < cit.cities.length; i++){
         console.log(chalk.blue("name:          " + cit.cities[i].name));
         console.log(chalk.red("country:       " + cit.cities[i].country));
         console.log(chalk.green("population:    " + cit.cities[i].population));
         console.log("-----------------------------");
     }
+
+    return cit;
+}
+
+function sortCities(citJSON) {
+    citJSON.cities.sort(function (a, b) {
+        if (a.population > b.population) {
+            return 1;
+        }
+        if (a.population < b.population) {
+            return -1;
+        }
+        // a muss gleich b sein
+        return 0;
+    });
 }
