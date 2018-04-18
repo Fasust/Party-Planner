685be2a9384d8296daa17e189d0a220e8d151268
Sebastian Faust <sebastianf333@googlemail.com>
2018-04-18 13:45:13 +0200

    added Firebade module init

--- main.js
+++ main.js
@@ -5,6 +5,14 @@
  ************************************************************************/
 
 var FS_MOD = require('fs');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("/wba2fts-firebase-admin.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://wba2fts.firebaseio.com"
+});
 
 /************************************************************************
  * Main
