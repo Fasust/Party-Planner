f78cc3f5bd47949ec997d774d44c983f75322dff
Sebastian Faust <sebastianf333@googlemail.com>
2018-05-04 13:33:06 +0200

    Added Task 3_3 comments

--- Tasks/Task3/task3_3.js
+++ Tasks/Task3/task3_3.js
@@ -3,10 +3,11 @@ var array = [4, 1];
 console.log(sortInteger(array));
 
 function sortInteger(array) {
-    if(array[0] > array[1]){
-        array[0] += array[1];
-        array[1] = array[0] - array[1];
-        array[0] -= array[1];
+    if(array[0] > array[1]){  //wenn erste zahl grösser als zweite..
+
+        array[0] += array[1]; //addiere zweite zahl auf erste
+        array[1] = array[0] - array[1]; // //setze zweite zahl auf die differnce von der ersten und der zweiten zahl
+        array[0] -= array[1]; // ziehe zweite zahl von erster zahl ab
     }
     return array;
 }
