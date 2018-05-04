7542ce98a04378b3cf315329671c36ddc4bcbdac
Arthur Tissen (inf345) <tissen.arthur@gmail.com>
2018-05-04 12:58:35 +0200

    edited task 3.1

--- Tasks/Task3/task3.js
+++ Tasks/Task3/task3.js
@@ -7,3 +7,12 @@ for (var x = 0; x < 10; x++) {
         console.log("The number is " +x);
         }, 1000);
 }
+
+/* Gedachte ausgabe:
+0
+The number is 0
+wait 1 second
+1
+The number is 1
+...
+*/
