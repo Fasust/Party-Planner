db0c632f679c751df57bfaca7ca6cb048e6e9a68
Sebastian Faust <sebastianf333@googlemail.com>
2018-06-04 11:32:56 +0200

    Moved "Dienstnutzer" into separate folder Added Base Express implementation in main Roted /users to separate users.js Added Firebase JSON Request Methodes
    Added Users/ GET
    Added Users/ PUT
    Added Users/{id} /GET

---
+++ Dienstgeber/main.js
@@ -0,0 +1,73 @@
+/* WBA2 Gruppe Sebastian Faust, Arthur Tissen, Julian Schoemaker */
+
+/************************************************************************
+ * Load modules
+ ************************************************************************/
+
+//firestore
+const fs = require('fs');
+const admin = require("firebase-admin");
+const serviceAccount = require("../wba2ftsKey.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://wba2fts.firebaseio.com"
+});
+const db = admin.firestore();
+
+//Express
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+const PORT = 3000;
+
+//Routes
+const usersRouter = require('./routes/users');
+
+/************************************************************************
+ * Main
+ ************************************************************************/
+
+initExpress();
+expressExampels();
+setRoutes();
+
+/************************************************************************
+ * Functions
+ ************************************************************************/
+function initExpress() {
+
+    app.listen(PORT, function () {
+        console.log('--Listening on port 3000--');
+    });
+
+    //Error Handler
+    app.use(function(err, req, res, next) {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    });
+
+    //Log
+    app.use(function(req, res, next) {
+        console.log("Time: \t" + Date.now() + "\t| Request-path:\t" + req.path);
+        next();
+    });
+}
+function expressExampels() {
+
+    app.get('/', function (req, res) {
+        res.send('Hello World!');
+    });
+
+    app.post('/', function (req, res) {
+        res.send('Got a POST request');
+    });
+
+}
+function setRoutes() {
+    app.use('/users',usersRouter);
+}
---
+++ Dienstgeber/routes/users.js
@@ -0,0 +1,88 @@
+/************************************************************************
+ * Load modules
+ ************************************************************************/
+
+//firestore
+const admin = require("firebase-admin");
+const db = admin.firestore();
+
+//Express
+var express = require('express');
+var router = express.Router(null);
+
+const ROUTE = "users";
+
+/************************************************************************
+ * Main
+ ************************************************************************/
+
+//GET
+router.get('/', function (req, res) {
+    getCollectionAsJSON(ROUTE).then(result => res.json(result));
+});
+router.get('/:uid' ,function (req, res) {
+    var userId = req.params.uid;
+    getDokumentAsJSON(ROUTE,userId).then(result => res.json(result));
+});
+
+//POST
+router.post('/', function (req, res) {
+    //get JSON atributes from req
+    var user_name = req.body.name;
+
+    //build them into a JSON
+    var user = {
+        name : user_name
+    }
+
+    //POST them in Firebase
+    var id = getIdInCollection(ROUTE);
+    db.collection(ROUTE).doc(id).set(user);
+
+    //Send the URI of new User
+    res.send("http://localhost:3000/" + ROUTE + "/" + id);
+});
+
+//Export as Module
+module.exports = router;
+
+/************************************************************************
+ * Functions
+ ************************************************************************/
+
+function getIdInCollection(collectionName) {
+    var ref = db.collection(collectionName).doc();
+    var id = ref.id;
+
+    return id;
+}
+function getCollectionAsJSON(collectionName) {
+    return new Promise(function (resolve) {
+        var json = {};
+
+        var collection = db.collection(collectionName);
+        collection.get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+
+                    json[doc.id] = doc.data();
+                });
+            }).then(function () {
+                resolve(json);
+            });
+    });
+}
+function getDokumentAsJSON(collectionName,docName) {
+    return new Promise(function (resolve) {
+        var json = {};
+
+        var document = db.collection(collectionName).doc(docName);
+        document.get()
+            .then(doc => {
+                json = doc.data();
+
+            }).then(function () {
+                resolve(json);
+            });
+    });
+}
