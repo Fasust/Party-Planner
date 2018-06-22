cd36c16a87416a6cff5c1348c6b36d0a9e623b5a
Sebastian Faust <sebastianf333@googlemail.com>
2018-06-22 14:11:06 +0200

    Installed request module and added it to package

--- Dienstnutzer/main.js
+++ Dienstnutzer/main.js
@@ -4,6 +4,8 @@
  * Load modules
  ************************************************************************/
 
+const request = require('request');
+
 //Express
 const express = require('express');
 const app = express();
@@ -28,7 +30,7 @@ expressExampels();
 function initExpress() {
 
     app.listen(PORT, function () {
-        console.log('--Listening on port 3000--');
+        console.log('--Listening on port 4000--');
     });
 
     //Error Handler
