da0dae342df9d5c6036b0d47d5049861c2c6660c
Fasust <sebastianf333@googlemail.com>
2018-07-01 09:15:02 +0200

    Fixed "//" in URI - Navigation Events

--- Dienstgeber/routes/events.js
+++ Dienstgeber/routes/events.js
@@ -36,9 +36,9 @@ router.post('/', function (req, res) {
     let eventID = getIdInCollection(ROUTE);
 
     //Adding Links to child domains
-    let wishUri = req.protocol + '://' + req.get('host') + req.originalUrl + "/" + eventID + "/wishes";
-    let shoppingUri = req.protocol + '://' + req.get('host') + req.originalUrl + "/" + eventID + "/shoppinglist";
-    let userUri = req.protocol + '://' + req.get('host') + req.originalUrl + "/" + eventID + "/users";
+    let wishUri = req.protocol + '://' + req.get('host') + req.originalUrl + eventID + "/wishes";
+    let shoppingUri = req.protocol + '://' + req.get('host') + req.originalUrl + eventID + "/shoppinglist";
+    let userUri = req.protocol + '://' + req.get('host') + req.originalUrl + eventID + "/users";
     event.navigation = {
         "users" : userUri,
         "shoppinglist" : shoppingUri,
